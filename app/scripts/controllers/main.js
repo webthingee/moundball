9ccf23a180b6dbd440e8746ac28e32e2c471336a
Sean Lange <sean@webthingee.com>
2013-04-29 21:05:51 -0700

    update functionallity to include functions

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,43 +10,84 @@ angular.module('moundballApp')
   })
   .controller('Ballerz', function ($scope) {
     $scope.ballerz = [
-      { name: 'Joe Doe', row: '21', seat: '20', total: 0 },
-      { name: 'Paul Irving', row: '21', seat: '19', total: 1 },
-      { name: 'Matt Smith', row: '21', seat: '18', total: 3 },
-      { name: 'Jane Molley', row: '21', seat: '17', total: -5 }
+      { name: 'Julie', row: '21', seat: '20', total: 0 },
+      { name: 'Dave', row: '21', seat: '19', total: 0 },
+      { name: 'Malia', row: '21', seat: '18', total: 0 },
+      { name: 'Sean', row: '21', seat: '17', total: 0 }
     ];
 
     $scope.ballerTurn = 0;
-    $scope.points = 1;
+    $scope.points = 0;
 
     // ballerTimer
 
-    $scope.currentInning = '1';
-    $scope.count = 0;
     $scope.increment = function() { $scope.count++; };
-    $scope.givePoints = function() { givePoints($scope) };
+    $scope.halfInning = function() { halfInning($scope); };
+    $scope.onMound = function() { onMound($scope) };
+    $scope.offMound = function() { offMound($scope) };
   });
 
-function givePoints($scope) {
-  var allBallerz = $scope.ballerz.length;
-  var ballerIndex = $scope.ballerTurn % $scope.ballerz.length;
-  var ballerCurrent = $scope.ballerz[ballerIndex];
-  var ballerCurrentTotal = $scope.ballerz[ballerIndex].total;
-  $scope.ballerz[ballerIndex].total = ballerCurrentTotal + $scope.points;
 
-  for(var i=1; i < allBallerz; i++) {
-    if (ballerIndex != i) {
-      console.log($scope.ballerz[i].name + ' : ' + $scope.ballerz[i].total);
-      $scope.ballerz[i].total = (-1 + $scope.ballerz[i].total);
-    }
+function halfInning($scope) {
+  // Change to correct half inning.
+  //
+  // Take one point from each baller.
+  for(var i=0; i < $scope.ballerz.length; i++) {
+    $scope.ballerz[i].total = (-1 + $scope.ballerz[i].total);
+  }
+  // Set current point award.
+  $scope.points = $scope.points + $scope.ballerz.length;
+
+  // Count.
+  $scope.counter = $scope.counter + 1;
+  $scope.homeAway = $scope.conunter % 2;
+
+  if($scope.counter % 2) {
+    $scope.homeAway = "Top of the ";
+    $scope.inning = $scope.inning + 1;
+  } else {
+    $scope.homeAway = 'Bottom of the ';
   }
 
-  $scope.ballerTurn = $scope.ballerTurn +1;
-  console.log(ballerCurrent.name);
-  console.log(ballerCurrentTotal + $scope.points);
 }
 
-function halfInningChange($scope) {
-  $scope.ballerTurn = $scope.ballerTurn + 1;
-  $scope.points = $scope.points + 1;
+function offMound($scope) {
+  halfInning($scope);
+
+  // Advance to next baller.
+  $scope.ballerTurn = $scope.ballerTurn +1;
+}
+
+function onMound($scope) {
+  // The number of ballerz.
+  var allBallerz = $scope.ballerz.length;
+  // The current position, based on the current turn.
+  var ballerIndex = $scope.ballerTurn % $scope.ballerz.length;
+  // The current baller.
+  var ballerCurrent = $scope.ballerz[ballerIndex];
+  // The point total of the current baller.
+  var ballerCurrentTotal = $scope.ballerz[ballerIndex].total;
+
+  // Cycle through all the batters to perfrom actions for non winners.
+  // for(var i=1; i < allBallerz; i++) {
+    // All who are NOT the current baller.
+    // if (ballerIndex != i) {
+      // console.log($scope.ballerz[i].name + ' : ' + $scope.ballerz[i].total);
+      // Take one point away from their total.
+      // $scope.ballerz[i].total = (-1 + $scope.ballerz[i].total);
+    // }
+  // }
+
+  // Actions for winner
+  $scope.ballerz[ballerIndex].total = ballerCurrentTotal + $scope.points;
+  $scope.points = 0;
+
+  // Advance to next baller.
+  $scope.ballerTurn = $scope.ballerTurn +1;
+
+  // Add new points.
+  halfInning($scope);
+
+  console.log(ballerCurrent.name);
+  console.log(ballerCurrentTotal + $scope.points);
 }
